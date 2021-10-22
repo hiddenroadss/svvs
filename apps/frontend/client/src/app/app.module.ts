45d5e5a1ec93d4dcbe1ae1dacbe421397edbb603
Taras Savchenko <taras.savchenko@jelvix.com>
2021-10-23 00:21:24 +0300

    feat(scss): set imports
    * set imports in ws-styles and styles folders
    * add test code to check work
    
    #10

--- apps/frontend/client/src/app/app.module.ts
+++ apps/frontend/client/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, MatButtonModule, MatCardModule],
   providers: [],
   bootstrap: [AppComponent],
 })
